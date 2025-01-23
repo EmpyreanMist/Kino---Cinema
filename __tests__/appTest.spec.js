11f43728dcfcfa8a03812d21363f9d436989b644
Christian Fryksten <christianfryksten1@gmail.com>
2025-01-23 12:00:56 +0100

    Add text that cheks a non existing movie

--- __tests__/appTest.spec.js
+++ __tests__/appTest.spec.js
@@ -10,11 +10,18 @@ describe("API Tests", () => {
   });
 
   // Testar specifika filmer
-  it("should return the correct movie details for a valid id", async () => {
+  it("Should return the correct movie details for a valid id", async () => {
     const response = await request(app).get("/movies/1"); // Kollar specifikt på första filen
     expect(response.status).toBe(200);
     expect(response.text).toContain("Isle of dogs"); // Kollar om första film sidan innehålller texten Pulp fiction
     expect(response.text).toContain("<img"); // Kollar att en img tagg finns med
     expect(response.text).toContain("<p"); // Kollar om en p tagg finns
+  });
+
+  // Testar för en film som inte finns
+  it("Should return 404 page for movies that doesn't exist in API", async () => {
+    const response = await request(app).get("/movies/1337"); // Testar film nummer LEET
+    expect(response.status).toBe(404); // Förväntar sig en 404 kod
+    expect(response.text).toContain("Movie not found!"); // Kollar att texten finns på sidan
   });
 });
